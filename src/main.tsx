033de9398b6006741112d83254c5c58e34a45f05
Anass-Dr <anasdrissi77@gmail.com>
2025-03-17 17:45:26 +0000

    Add AuthProvider for Cognito authentication in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { AuthProvider } from "react-oidc-context";
 
-createRoot(document.getElementById('root')!).render(
+const cognitoAuthConfig = {
+  authority:
+    "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_HADFOguim",
+  client_id: "78vo9c43blmbjguo0rsrtsnog6",
+  redirect_uri: "http://localhost:5173",
+  response_type: "code",
+  scope: "email openid phone",
+};
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
